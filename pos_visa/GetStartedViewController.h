07abecbe7d94b18032fb1788540923225e6cf286
tracylakinprosper <tlakin@prosper.com>
2015-08-11 18:25:56 -0700

    First Commit of POS - Visa - Prosper prototype app. A couple of screens started, basic view flow.

---
+++ pos_visa/GetStartedViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetStartedViewController.h
+//  pos_visa
+//
+//  Created by Tracy Lakin on 8/11/15.
+//  Copyright (c) 2015 Prosper Marketplace, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetStartedViewController : UINavigationController
+
+@end
